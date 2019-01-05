8ea7daff3f482ca9c01d680dc378ee1236e57ac6
Brandon Sam Soon <brandon.samsoon@loblaw.ca>
2019-01-05 17:18:28 -0500

    deps changes

--- mix.exs
+++ mix.exs
@@ -24,8 +24,7 @@ defmodule Ptolemy.MixProject do
       {:tesla, "~> 1.2.1"},
       {:joken, "~> 1.5"},
       {:hackney, "~> 1.6"},
-      {:jason, ">= 1.0.0"},
-      {:poison, "~> 3.1"}
+      {:jason, ">= 1.0.0"}
     ]
   end
 end
