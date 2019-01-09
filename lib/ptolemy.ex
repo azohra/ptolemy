5a90673c2c3745c2fac4640ecb74afc5f60c2874
Brandon Sam Soon (LCL) <brandon.samsoon@loblaw.ca>
2019-01-09 17:47:24 -0500

    doc changes

--- lib/ptolemy.ex
+++ lib/ptolemy.ex
@@ -6,71 +6,90 @@ defmodule Ptolemy do
     In order to properly use ptolemy, you must properly set a ptolemy configuration block.
 
     The available configuration option are as follows:
-    - `:vault_url` (Required) - The url of the remote vault server.
-    - `:auth_mode` (Required) - The authentication method that ptolemy will try to do. As of `0.1.0`, `"GCP"` and `"approle"` are the only supported authentication methods.
+    - `:vault_url` ::string (Required) - The url of the remote vault server.
+
+    - `:auth_mode` ::string (Required) - The authentication method that ptolemy will try to do. As of `0.1.0`, `"GCP"` and `"approle"` are the only supported authentication methods.
+    
     - `:kv_engine` ::map - The kv engine block configuration, all your kv configuration should be store in here.
-      - `:engine_path` ::string - The engine's path, always need to start with the name and end with a `/`.
-    - `:credentials`
-    - `:opts`
-  
+      - `:engine_path` ::string 
+        - The engine's path, always need to start with the name and end with a `/`.
+      - `:secrets` ::map 
+        - The path of each secrets you wish to use. Note each 
+    
+    - `:credentials` ::map (Required) - The sets of credentials that will be used to authenticate to vault
+      - If you are using the Approle auth method:
+        - `:role_id` ::string 
+          - The role ID to use to authenticate.
+        - `:secred_id` ::string 
+          - The secret ID to use to authenticate.
+      - If you are using the GCP auth method:
+        - `:svc_acc` ::Based64 encoded string 
+          - The Google service account used to authenticate through IAP and/or vault.
+    
+    - `:opts` ::List - Optional list.
+      - `:iap_on` ::boolean
+        - Sets whether the remote vautl server has IAP protection or not. If you are using GCP auth method you must provide
+        a service account credential with both `Service Account Token Creator` and `Secured IAP User`. If you are using the Approle auth method
+        you must provide `:svc_acc` and a `:target_audience` (client_id of the IAP protected resource) within the `:credential` block.
+      - `:exp` ::integer 
+        - The expiry of each access token (IAP - if enabled - and the vault token). The value has a default of 900 seconds(15 min), keep in mind
+        the maximum time allowed for any google tokens is 3600 (1 hour), for vault that is entirely depended on what the administrator sets (default is 15min).
+
   ## Configuration Examples: 
     - For an approle configuration
     ```elixir
-    production: %{
-      vault_url: "http://localhost:8200",
-      auth_mode: "approle",
-      kv_engine: %{
-        kv_engine1: %{
-          engine_path: "secret/",
-          secrets: %{
-            ptolemy: "/ptolemy"
+    config :ptolemy, Ptolemy,
+      production: %{
+        vault_url: "http://localhost:8200",
+        auth_mode: "approle",
+        kv_engine: %{
+          kv_engine1: %{
+            engine_path: "secret/",
+            secrets: %{
+              ptolemy: "/ptolemy"
+            }
           }
-        }
-      },
-      credentials: %{
-        role_id: System.get_env("ROLE_ID"),
-        secret_id: System.get_env("SECRET_ID")
-      },
-      opts: [
-        iap_on: false,
-        exp: 6000
-      ]
-    }
+        },
+        credentials: %{
+          role_id: System.get_env("ROLE_ID"),
+          secret_id: System.get_env("SECRET_ID")
+        },
+        opts: [
+          iap_on: false,
+          exp: 6000
+        ]
+      }
     ```
     - For a GCP configuration
     ```elixir
-    production: %{
-      vault_url: "http://localhost:8200",
-      auth_mode: "GCP",
-      kv_engine: %{
-        kv_engine1: %{
-          engine_path: "secret/",
-          secrets: %{
-            ptolemy: "/ptolemy"
+    config :ptolemy, Ptolemy,
+      production: %{
+        vault_url: "http://localhost:8200",
+        auth_mode: "GCP",
+        kv_engine: %{
+          kv_engine1: %{
+            engine_path: "secret/",
+            secrets: %{
+              ptolemy: "/ptolemy"
+            }
           }
-        }
-      },
-      credentials: %{
-        svc_acc: System.get_env("GOOGLE_SVC_ACC"),
-        target_audience: System.get_env("TARGET_AUD")
-      },
-      opts: [
-        iap_on: false,
-        exp: 6000
-      ]
-    }
+        },
+        credentials: %{
+          svc_acc: System.get_env("GOOGLE_SVC_ACC"),
+          target_audience: System.get_env("TARGET_AUD")
+        },
+        opts: [
+          iap_on: false,
+          exp: 6000
+        ]
+      }
     ```
-
-  ## Gotchas
-    If IAP is enabled via `:iap_on` option, you must provide a GCP service account credential with access to the IAP resource
-    aaaa
   """
   alias Ptolemy.Server
   alias Ptolemy.Engines.KV
 
   @doc """
-  Starts a process that holds a specific vault server configuration. This state holds all necessary
-  information to connect, retrieve, update, etc... data on a vault server.
+  Entrypoint of ptolemy, this will start the process and store all necessary state for a connection to a remote vault server.
   """
   def start(name, config) do
     Server.start(name, config)
