c95b5285a9790c791dd9dec43f19291555436400
Brandon Sam Soon (LCL) <brandon.samsoon@loblaw.ca>
2019-01-15 11:01:28 -0500

    removed test configs

--- config/config.exs
+++ config/config.exs
@@ -3,26 +3,3 @@
 use Mix.Config
 
 config :tesla, adapter: Tesla.Adapter.Hackney
-
-config :ptolemy, Ptolemy,
-  server1: %{
-    vault_url: "http://localhost:8200",
-    auth_mode: "approle",
-    kv_engine: %{
-      kv_engine1: %{
-        engine_path: "secret/",
-        secrets: %{
-          ptolemy: "/ptolemy"
-        }
-      }
-    },
-    credentials: %{
-      role_id: System.get_env("ROLE_ID"),
-      secret_id: System.get_env("SECRET_ID")
-    },
-    opts: [
-      role: "default",
-      iap_on: false,
-      exp: 6000
-    ]
-  }
