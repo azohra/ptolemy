b5823eb1d42f278df8ddc6a9836b98394246a758
tangsaidi <tangsaidi@icloud.com>
2019-03-06 15:22:33 -0500

    Added create role set and delete

--- lib/engines/gcp.ex
+++ lib/engines/gcp.ex
@@ -8,6 +8,7 @@ defmodule Ptolemy.Engines.GCP do
     @doc """
     Generate a service account key, this is equiv to read service account key. 
     For create operation, please use `create_roleset`.
+    The roleset variable should be an entire path  /gcp/roleset/my-key-roleset
     """
     def generate_service_account_key(client, roleset, key_alg \\ "KEY_ALG_RSA_2048", key_type \\ "TYPE_GOOGLE_CREDENTIALS_FILE" ) do
         payload = %{key_algorithm: key_alg, key_type: key_type}
@@ -17,7 +18,7 @@ defmodule Ptolemy.Engines.GCP do
                 {status, _} when status in 200..299 ->
                     status
                 {status, msg} -> 
-                    throw "Could not generate service account key.\nError code: #{status}\nError message: #{msg}"
+                    throw "Could not generate service account key.\nError code: #{status}"
             end
         end
     end
@@ -25,6 +26,7 @@ defmodule Ptolemy.Engines.GCP do
     @doc """
     Generate an access token, this is equiv to read oath2 token opertaions.
     For create operation, please use `create_roleset`.
+    The roleset variable should be an entire path  /gcp/roleset/my-token-roleset
     """
     def generate_service_token(client, roleset) do
         with {:ok, resp} <- Tesla.get(client, "#{roleset}") do
@@ -32,7 +34,7 @@ defmodule Ptolemy.Engines.GCP do
                 {status, _} when status in 200..299 ->
                     status
                 {status, msg} -> 
-                    throw "Could not generate service account key.\nError code: #{status}\nError message: #{msg}"
+                    throw "Could not generate access token.\nError code: #{status}"
             end
         end
     end
@@ -41,19 +43,46 @@ defmodule Ptolemy.Engines.GCP do
     @doc """
     Create/Update Roleset
     Accepted secret_type values: "access_token", "service_account_key"
-
     """
-    #def create_roleset(client, name, secret_type) do
-    #    payload = %{key_algorithm: key_alg, key_type: key_type}
+    def create_roleset(client, name, project, bindings, secret_type \\ "access_token", token_scopes \\ []) do
+       payload = case secret_type do
+           "access_token" -> 
+                %{secret_type: secret_type,
+                 project: project,
+                 token_scopes: token_scopes,
+                 bindings: bindings
+                }
+           "service_account_key" ->
+                %{secret_type: secret_type,
+                project: project,
+                bindings: bindings
+                }
+       end
 
-    #    with {:ok, resp} <- Tesla.post(client, "#{roleset}", payload) do
-    #        case {resp.status, resp.body} do
-    #            {status, _} when status in 200..299 ->
-    #                status
-    #            {status, msg} -> 
-    #                throw "Could not generate service account key.\nError code: #{status}\nError message: #{msg}"
-    #        end
-    #    end
-    #end
+       with {:ok, resp} <- Tesla.post(client, "#{name}", payload) do
+           case {resp.status, resp.body} do
+               {status, _} when status in 200..299 ->
+                   status
+               {status, msg} -> 
+                   throw "Could not create role set.\nError code: #{status}"
+           end
+       end
+    end
+
+    @doc """
+    Delete Roleset, this only support service account keys
+    """
+    def delete_roleset(client, lease_id) do
+        payload = %{lease_id: lease_id}
+ 
+        with {:ok, resp} <- Tesla.put(client, "/sys/leases/revoke", payload) do
+            case {resp.status, resp.body} do
+                {status, _} when status in 200..299 ->
+                    status
+                {status, msg} -> 
+                    throw "Could not delete role set.\nError code: #{status}"
+            end
+        end
+     end
 
 end
