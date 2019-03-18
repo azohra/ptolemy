462e5d5f7a5a56342948195bb3e43f2c89d18be8
Borna Houmani-Farahani (LCL) <borna.houmani-farahani5@loblaw.ca>
2019-03-18 16:20:38 -0400

    roleset builders

--- lib/engines/gcp/gcp.ex
+++ lib/engines/gcp/gcp.ex
@@ -1,8 +1,23 @@
-defmodule Ptolemy.Engines.GCP.Engine do
+defmodule Ptolemy.Engines.GCP do
   @moduledoc """
   `Ptolemy.Engines.GCP` provides interaction with Vault's Google Cloud Secrets Engine.
+
+  {:app_name, :another_secret, {GCP.Provider, {gcp_secret_type}}}
+
   """
   require Logger
+  alias Ptolemy.Server
+  alias Ptolemy.Engines.GCP.Engine
+
+  @typedoc """
+  Types of Google Cloud Secrets allowed by Vault. see
+  """
+  @type gcp_secret_type :: :access_token | :service_account_key
+
+  def create_roleset(pid, engine_name, token_name, payload \\ test_payload()) do
+    create_client(pid, engine_name)
+    |> Engine.create_roleset(token_name, payload)
+  end
 
   def test_payload() do
     %{
@@ -15,4 +30,48 @@ defmodule Ptolemy.Engines.GCP.Engine do
       ]
     }
   end
+
+  #Tesla client function
+  def create_client(pid, engine_name) do
+    creds = Server.fetch_credentials(pid)
+    {:ok, url} = Server.get_data(pid, :vault_url)
+    {:ok, engines} = Server.get_data(pid, :engines)
+
+    engine_path = engines
+    |> Map.fetch!(engine_name)
+    |> Map.fetch!(:engine_path)
+
+    IO.inspect Tesla.client([
+      {Tesla.Middleware.BaseUrl, "#{url}/v1/#{engine_path}"},
+      {Tesla.Middleware.Headers, creds},
+      {Tesla.Middleware.JSON, []}
+    ])
+  end
+
+  @moduledoc """
+   ## Fields
+
+    * `:secret_type` - type of secret generated for this role set. i.e. "access_token", "service_account_key"
+    * `:project` - name of the GCP project to which this roleset's service account will belong 
+    * `:bindings` - bindings configuration string (read more here: https://www.vaultproject.io/docs/secrets/gcp/index.html#roleset-bindings)
+    * `:token_scopes` - *Applies only if secret type is "access_token"** list of OAuth scopes to assign to secrets generated under this role set
+  """
+  def create_roleset(:access_token, project, bindings, scopes) do
+    %{
+      secret_type: "access_token",
+      project: project,
+      bindings: bindings,
+      token_scopes: scopes
+    }
+  end
+
+  def create_roleset(:service_account_key, project, bindings) do
+    %{
+      secret_type: "service_account_key",
+      project: project,
+      bindings: bindings
+    }
+  end
 end
+
+
--- lib/engines/gcp/gcp_engine.ex
+++ lib/engines/gcp/gcp_engine.ex
@@ -4,20 +4,24 @@ defmodule Ptolemy.Engines.GCP.Engine do
   """
   require Logger
 
+  # client iff server
+
+
   def read_roleset(client, name)
-  def list_roleset(client, name)
   def rotate_roleset(client, name) #equivalent to invalidating the tokens under that roleset
 
   def create_roleset(client, name, payload)
   def create_token_roleset(client, name, project, bindings, scopes)
-  def create_key_roleset(client, name, project, bindings, scopes)
+  def create_svc_acc_roleset(client, name, project, bindings)
 
   def gen_token(client, roleset_name)
   def gen_key(client, roleset_name)
 
-
+  # client (server)  vvvvIn the configvvvvv
+  # path (token type, token name)
+  # payload (project, binding, scopes)
   def create_roleset(client, name, payload) do
-    Tesla.post(client, "/gcp/roleset/#{name}", payload)
+    Tesla.post(client, "/roleset/#{name}", payload)
   end
 
 end
