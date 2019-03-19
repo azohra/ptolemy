438a564d991b0f6d54cdf2070abbe9e4d4e8e7dd
Brandon Sam Soon <brandon.samsoon@loblaw.ca>
2019-03-19 09:28:10 -0400

    Readme update xD, travis addition, fixed warnings.

--- README.md
+++ README.md
@@ -1,20 +1,24 @@
 <p align="center"> 
   <img src="./assets/logo.svg" height="100%" width="100%"/>
+  <a href= "https://travis-ci.org/azohra/ptolemy"><img src="https://travis-ci.org/azohra/ptolemy.svg?branch=master"></a>
+  <a href= "https://hex.pm/packages/ptolemy"><img src="https://img.shields.io/hexpm/v/ptolemy.svg"/></a>
+  <img src="https://img.shields.io/hexpm/l/ptolemy.svg"/>
 </p>
 
 ---
-Ptolemy is an application that allows your elixir app to use [hashicorp's vault](https://github.com/hashicorp/vault) to store and manage application secrets. Designed with simplicity in mind, ptolemy simplifies the vault api and features advanced features such as authentication through cloud iap.
+Ptolemy is an application environment manager. It can dynamically load specified application envs from a remote backend such as Hashicorp's Vault and handle its lifecycle. 
 
-## Currently Supported Features
-- Authenticates to vault via:
-  - **Google Cloud** auth method
-  - **Approle** auth method
-
+## Features
+- [Application environment management](https://hexdocs.pm/ptolemy/0.2.0/Ptolemy.Loader.html#content). 
 - Authentication through Google's [Cloud IAP](https://cloud.google.com/iap/)
-
-- Supported secret engines
-  - KV V2 secret engine
-  - More to come!
+- [Hashicorp Vault](https://github.com/hashicorp/vault) integration:
+  - Supported authentication methods:
+    - GCP
+    - Approle
+  - Supported secret engines:
+    - KV V2 
+    - PKI (WIP)
+    - GCP (WIP)
 
 ## Installation
 Ptolemy is available on hex you can install it by following these steps:
@@ -23,61 +27,55 @@ Ptolemy is available on hex you can install it by following these steps:
 ```elixir
 def deps do
   [
-    {:ptolemy, "~> 0.1.0"}
+    {:ptolemy, "~> 0.2.0"}
   ]
 end
 ```
 2. Run `mix deps.get && mix deps.compile`
 
-## Usage
-You will first need to edit your `config/config.exs` to include a configuration (visit `Ptolemy` module to find more configuration options) block such as:
+## Configuration
+
+There are two configuration blocks that exist in ptolemy as of version `0.2`. Both are independent of one another and do not have to be used at the same time, however to get the most out of this library you would want to configure both. 
+
+### Configuring `:vaults`
+The `:vaults` key configure's ptolemy various backend providers(Hashicorp vault is the only backend currently supported). Each key within the `:vaults` block represents a specific server in which ptolemy can query to retrieve values such as application secrets currently stored in vault.
+
 ```elixir
-config :ptolemy, Ptolemy,
-  server1: %{
-    vault_url: "http://localhost:8200",
-    auth_mode: "approle",
-    kv_engine: %{
-      kv_engine1: %{
-        engine_path: "secret/",
-        secrets: %{
-          ptolemy: "/ptolemy"
-        }
+config :ptolemy, vaults: %{
+  server_name: %{
+    vault_url: "",
+    auth: %{
+      method: :Approle,
+      credentials: %{},
+      opts: []
+    },
+    engines: [
+      engine_name: %{
+        type: :kv2,
+        path: "/path",
+        #....
       }
-    },
-    credentials: %{
-      role_id: System.get_env("ROLE_ID"),
-      secret_id: System.get_env("SECRET_ID")
-    },
-    opts: [
-      iap_on: false,
-      exp: 6000
     ]
-  }.
-  server2: %{...}
-  ```
+  }
+  # server_name2: %{...}
+}
+```
+Additional details about usage the keys within the `:vaults` configuration block can be found it the [documentations](https://hexdocs.pm/ptolemy/0.2.0/Ptolemy.html#content). 
 
-  It is recommended that you start a ptolemy process via a supervised process such as:
-  ```elixir
-  def application do
-    worker(Ptolemy.Server, [:pid, :server1]),
-  end
-  ```
-  Additionally you could provide runtime configuration by specifying an extra keyword list when you start Ptolemy.
-  ```elixir
-  def application do
-    worker(Ptolemy.Server, [:pid, :server1, [credentials: %{role_id: "test", secret_id: "test"}, opts: []]]),
-  end
-  ```
+### Configuring `:loader`
+If `Ptolemy.Loader` is being used to dynamically manage appplication environment variables, then an extra configuration block should also be added:
 
- Once you configure and start the application, you have access to simple getters and setters, like: 
-  ```elixir
-  iex(1)> {:ok, server} = Ptolemy.start(:production, :server1)
-  {:ok, <#PID<0.213.0>}}
-  iex(2)> server |> Ptolemy.kv_cread(:kv_engine1, :ptolemy, "foo")
-  {:ok, "test"} # not a real secret, or is it???? 乁( ͡° ͜ʖ ͡°)ㄏ
-  iex(3)> server |> Ptolemy.kv_read("secret/data/ptolemy", "foo")
-  {:ok, "test"} 
-  ```
+Sepcifying this block will allow `Ptolemy.Loader` to populate the application specific env vars at runtime.
+
+```elixir
+  config :ptolemy, loader: [
+    env: [
+      {{:app_name, :secret_key}, {SystemEnv, "PATH"}},
+      # ...
+    ]
+  ]
+```
+Additional details about usage the keys within the `:loader` configuration block can be found it the [documentations](https://hexdocs.pm/ptolemy/0.2.0/Ptolemy.Loader.html#content). 
 
 ## Development
   Running a local dev environment of ptolemy requires:
@@ -95,28 +93,3 @@ config :ptolemy, Ptolemy,
   ```
 
   This will setup a local vault server accessible at `http://localhost:8200` along with setting up a the docker-composed vault server with a testing approle, the credentials for the role will be exported to your environment variable of the current shell used `SECRET_ID` and `ROLE_ID`.
-
-
-## Road Map
-
-1. Restructure Ptolemy to support genric engines
-2. Load secrets into application according to config file
-3. Improve performance of Vault fetch operations 
-4. Add engine support for
-  - Key/Value V2 (Done)
-    - The kv secret engine is used to store versioned key value pair secrets. Of course, you may use it as non-versioned if you wish. Our implementation contains a full set of operations to manipulate secrets. This engine is mostly done, thanks to @brsmsn
-  - GCP (WIP)
-    - This is a dynamic secret engine that generates short term Google Cloud service account keys and OAuth tokens based on IAM. It provides security advantages including short-term access and service key cleaning. Our support will provide account keys and tokens retrieval and updating functionalities.
-  - Cubbyhole (Planned)
-    - The cubbyhole secrets engine is used to store arbitrary secrets within the configured physical storage for Vault namespaced to a token. In cubbyhole, paths are scoped per token.
-  - PKI (Planned)
-    - The PKI secrets engine generates dynamic X.509 certificates.
-  - TOTP (Panned)
-    - The TOTP secrets engine generates time-based credentials according to the TOTP standard
-  - Databases (Planned)
-    - The database secrets engine generates database credentials dynamically based on configured roles.
-  - AWS (Bonus)
-    - TBD due to lack of internal demand, but AWS engine implementation will be similar to GCP engine's.
-5. Feel free to tell us what features do you want with Ptolemy by opening an issue
-
-Please find more details regarding our redesign in `docs/README.md`
--- lib/auth/auth.ex
+++ lib/auth/auth.ex
@@ -10,22 +10,22 @@ defmodule Ptolemy.Auth do
   ```elixir
   #Approle, no IAP
   Ptolemy.Auth.authenticate(:Approle, "https://test-vault.com", %{secret_id: "test", role_id; "test"}, [])
-  
+
   #Approle with IAP
   Ptolemy.Auth.authenticate(:Approle, "https://test-vault.com", %{secret_id: "test", role_id; "test"}, [iap_svc_acc:  @gcp_svc1_with_vault_perm, client_id: @fake_id, exp: 2000])
-  
+
   #Approle with IAP and `bearer` token being re-used
   Ptolemy.Auth.authenticate(:Approle, "https://test-vault.com", %{secret_id: "test", role_id: "test"}, {"Authorization", "Bearer 98a4c7ab98a4c7ab98a4c7ab"})
-  
+
   #GCP with no IAP
   Ptolemy.Auth.authenticate(:GCP, "https://test-vault.com", my_svc, [])
-  
+
   #GCP with IAP, 2 Google service accounts, one for vault one for IAP
   Ptolemy.Auth.authenticate(:GCP, @vurl, %{gcp_svc_acc: @gcp_svc1_with_vault_perm, vault_role: "test", exp: 3000}, [iap_svc_acc:  my_svc, client_id: @fake_id, exp: 2000])
-  
+
   #GCP with IAP, re-using the same GCP service account being used to authenticate to vault inorder to auth into IAP
   Ptolemy.Auth.authenticate(:GCP, @vurl, %{gcp_svc_acc: @gcp_svc1_with_vault_perm, vault_role: "test", exp: 3000}, [iap_svc_acc:  :reuse, client_id: @fake_id, exp: 2000])
- 
+
   #GCP with IAP and `bearer` token being re-used
   Ptolemy.Auth.authenticate(:GCP, @vurl, %{gcp_svc_acc: @gcp_svc1_with_vault_perm, vault_role: "test", exp: 3000}, {"Authorization", "Bearer 98a4c7ab98a4c7ab98a4c7ab"})
   ```
@@ -34,55 +34,53 @@ defmodule Ptolemy.Auth do
   @typedoc """
   Vault authentication data.
   """
-  @type vault_auth_data ::
-    %{
-      token: {String.t(), String.t()},
-      renewable: boolean(), 
-      lease_duration: pos_integer()
-    }
+  @type vault_auth_data :: %{
+          token: {String.t(), String.t()},
+          renewable: boolean(),
+          lease_duration: pos_integer()
+        }
 
   @typedoc """
   Google Identity Aware Proxy authentication data.
   """
-  @type iap_auth_data ::
-    %{
-      token: {String.t(), String.t()}
-    }
+  @type iap_auth_data :: %{
+          token: {String.t(), String.t()}
+        }
 
   @typedoc """
   Credential data needed to authenticated to a remote vault server.
 
   Each specific auth method's credential data have a different schema.
   """
-  @type cred_data :: 
-    %{ 
-      gcp_svc_acc: map(), 
-      vault_role: String.t(), 
-      exp: pos_integer()
-    } 
-    | %{
-        secret_id:  String.t(), 
-        role_id:  String.t()
-      }
+  @type cred_data ::
+          %{
+            gcp_svc_acc: map(),
+            vault_role: String.t(),
+            exp: pos_integer()
+          }
+          | %{
+              secret_id: String.t(),
+              role_id: String.t()
+            }
 
   @typedoc """
   Authentication options, used to specify IAP credentials and other future authentication options.
 
-  If under the `:iap_svc_acc` key `:reuse` is specified and the auth method was set to `:GCP`, `Ptolemy.Auth` 
-  will attempt to re-use the GCP service account specified under the supplied `cred_data` type. 
+  If under the `:iap_svc_acc` key `:reuse` is specified and the auth method was set to `:GCP`, `Ptolemy.Auth`
+  will attempt to re-use the GCP service account specified under the supplied `cred_data` type.
 
   `:client_id` is the OAuth2 client id, this can be found in Security -> Identity-Aware-Proxy -> Select the IAP resource -> Edit OAuth client.
-  
+
   `:exp` is the validity period for the token in seconds, google's API specifies that a token can only be valid for up to 3600 seconds.
 
   Specifying a tuple of type {"Authorization", "Bearer ....."} will notify `Ptolemy.Auth.authenticate/4` to reuse the token to prevent
   exessive auhtnetication calls to IAP.
   """
-  @type iap_auth_opts :: 
-    [] 
-    | [iap_svc_acc: map(), client_id: String.t(), exp: pos_integer()]
-    | [iap_svc_acc: :reuse, client_id: String.t(), exp: pos_integer()]
-    | {String.t(), String.t()}
+  @type iap_auth_opts ::
+          []
+          | [iap_svc_acc: map(), client_id: String.t(), exp: pos_integer()]
+          | [iap_svc_acc: :reuse, client_id: String.t(), exp: pos_integer()]
+          | {String.t(), String.t()}
 
   @typedoc """
   Atoms representing the authentication methods that is currently supported on ptolemy.
@@ -105,7 +103,8 @@ defmodule Ptolemy.Auth do
 
   Each modules representing a specific authentication method should implement this callback in its own module.
   """
-  @callback authenticate(endpoint :: String.t(), cred_data, iap_tok) :: vault_auth_data | {:error, String.t()}
+  @callback authenticate(endpoint :: String.t(), cred_data, iap_tok) ::
+              vault_auth_data | {:error, String.t()}
 
   @doc """
   Authenticates against a remote vault server with specified auth strategy and options.
@@ -114,32 +113,33 @@ defmodule Ptolemy.Auth do
 
   Note Specifying an empty list or a tuple to this function under `iap_auth_opts` will *NOT* return an IAP token and IAP credentials metadata.
   """
-  @spec authenticate(auth_method, String.t(), cred_data, iap_auth_opts) :: 
-    vault_auth_data 
-    | %{vault: vault_auth_data, iap: iap_auth_data} 
-    | {:error, String.t()}
+  @spec authenticate(auth_method, String.t(), cred_data, iap_auth_opts) ::
+          vault_auth_data
+          | %{vault: vault_auth_data, iap: iap_auth_data}
+          | {:error, String.t()}
   def authenticate(method, url, credentials, opts)
-  
-  #IAP not defined or not enabled
+
+  # IAP not defined or not enabled
   def authenticate(method, url, credentials, [] = opts) do
     auth(method, url, credentials, opts)
   end
 
-  #Re-use IAP Bearer tokens
-  def authenticate(method, url, credentials, {"Authorization", bearer} = opts) do
+  # Re-use IAP Bearer tokens
+  def authenticate(method, url, credentials, {"Authorization", _bearer} = opts) do
     auth(method, url, credentials, [opts])
   end
 
-  #IAP is enabled and has a seperate service account.
-  def authenticate(method, url, credentials, [iap_svc_acc: svc, client_id: cid, exp: exp]) when is_map(svc) do
+  # IAP is enabled and has a seperate service account.
+  def authenticate(method, url, credentials, iap_svc_acc: svc, client_id: cid, exp: exp)
+      when is_map(svc) do
     iap_tok = Ptolemy.Auth.Google.authenticate(:iap, svc, cid, exp)
     vault_tok = auth(method, url, credentials, [iap_tok])
 
     %{vault: vault_tok, iap: %{token: iap_tok}}
   end
 
-  #IAP is enabled with instruction to re-use `credentials` as the IAP service account
-  def authenticate(method, url, credentials, [iap_svc_acc: :reuse, client_id: cid, exp: exp]) do
+  # IAP is enabled with instruction to re-use `credentials` as the IAP service account
+  def authenticate(method, url, credentials, iap_svc_acc: :reuse, client_id: cid, exp: exp) do
     iap_tok = Ptolemy.Auth.Google.authenticate(:iap, credentials[:gcp_svc_acc], cid, exp)
     vault_tok = auth(method, url, credentials, [iap_tok])
 
@@ -160,16 +160,15 @@ defmodule Ptolemy.Auth do
       case {resp.status, resp.body} do
         {status, body} when status in 200..299 ->
           parse_vault_resp(body)
-          
+
         {status, body} ->
           message = Map.fetch!(body, "errors")
           {:error, "Authentication failed, Status: #{status} with error: #{message}"}
       end
-    else  
+    else
       err -> err
     end
   end
-
 
   @doc """
   Creates a `%Tesla.Client{}` pointing to a remote vault server.
@@ -183,7 +182,7 @@ defmodule Ptolemy.Auth do
     ])
   end
 
-  #parses auth body to return relevant information
+  # parses auth body to return relevant information
   defp parse_vault_resp(body) do
     %{
       "auth" => %{
@@ -193,6 +192,10 @@ defmodule Ptolemy.Auth do
       }
     } = body
 
-    %{token: {"X-Vault-Token", client_token}, renewable: renewable, lease_duration: lease_duration}
+    %{
+      token: {"X-Vault-Token", client_token},
+      renewable: renewable,
+      lease_duration: lease_duration
+    }
   end
 end
