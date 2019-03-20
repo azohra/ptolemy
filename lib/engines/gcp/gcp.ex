77d2d52977da9ce38e0c28e8a5891b10847e1464
Borna Houmani-Farahani (LCL) <borna.houmani-farahani5@loblaw.ca>
2019-03-20 16:14:18 -0400

    quick wrappers

--- lib/engines/gcp/gcp.ex
+++ lib/engines/gcp/gcp.ex
@@ -13,16 +13,104 @@ defmodule Ptolemy.Engines.GCP do
   """
   @type gcp_secret_type :: :access_token | :service_account_key
 
-  @spec create(pid, atom, binary, map) :: map
-  def create(pid, engine_name, token_name, roleset_payload \\ test_payload()) do
-    create_client(pid, engine_name)
-    |> Engine.create_roleset(token_name, roleset_payload)
+
+  @typedoc """
+    A GCP roleset map
+
+    ## Fields
+    * `:secret_type` - type of secret generated for this role set. i.e. "access_token", "service_account_key"
+    * `:project` - name of the GCP project to which this roleset's service account will belong
+    * `:bindings` - bindings configuration string (read more here: https://www.vaultproject.io/docs/secrets/gcp/index.html#roleset-bindings)
+    * `:token_scopes` - **Applies only if secret type is "access_token"** list of OAuth scopes to assign to secrets generated under this role set
+  """
+  @type roleset :: %{
+          required(:secret_type) => String.t(),
+          required(:project) => String.t(),
+          required(:bindings) => String.t(),
+          optional(:token_scopes) => list(String.t())
+        }
+
+  @doc """
+  Creates a role set in the given engine and returns `:ok` if everything goes well.
+  Throws an error message otherwise.
+  """
+  @spec create!(pid(), atom(), String.t(), roleset) :: :ok
+  def create!(pid, engine_name, roleset_name, roleset_payload \\ test_payload()) do
+    create(pid, engine_name, roleset_name, roleset_payload)
+    |> case do
+      {:ok, _body} -> :ok
+      {:error, err} -> throw(err)
+    end
   end
 
-  @spec read(pid, atom, gcp_secret_type, binary) :: map
+  @doc """
+  Generates an `access token`/`service account key` from the given roleset,
+  returning a map containing secret data and throwing an error of an issue occurs.
+  """
+  @spec read!(pid(), atom(), gcp_secret_type, String.t()) :: map()
+  def read!(pid, engine_name, secret_type, roleset_name) do
+    read(pid, engine_name, secret_type, roleset_name)
+    |> case do
+      {:ok, body} -> body
+      {:error, err} -> throw(err)
+    end
+  end
+
+  @doc """
+  """
+  @spec update!(pid(), atom(), String.t(), roleset) :: :ok
+  def update!(pid, engine_name, roleset_name, roleset_payload \\ test_payload()) do
+    update(pid, engine_name, roleset_name, roleset_payload)
+    |> case do
+      {:ok, _} -> :ok
+      {:error, err} -> throw(err)
+    end
+  end
+
+  @doc """
+  """
+  @spec delete!(pid(), atom(), gcp_secret_type, String.t()) :: :ok
+  def delete!(pid, engine_name, secret_type, roleset_name) do
+    delete(pid, engine_name, secret_type, roleset_name)
+    |> case do
+      {:ok, _} -> :ok
+      {:error, err} -> throw(err)
+    end
+  end
+
+  @doc """
+  Retreives the current configuration for a given roleset. The response can be used for
+  changing the bindings or scopes to then update the roleset. Throws an error if anything
+  goes wrong.
+  """
+  @spec read_roleset!(pid(), atom(), String.t()) :: roleset
+  def read_roleset!(pid, engine_name, roleset_name) do
+    create_client(pid, engine_name)
+    |> Engine.read_roleset(roleset_name)
+    |> case do
+      {:ok, body} -> body
+      {:error, err} -> throw(err)
+    end
+  end
+
+  @doc """
+  Creates a roleset in the given engine from which `access token`s and
+  `service account key`s can be generated.
+  """
+  @spec create(pid(), atom(), String.t(), roleset) :: {:ok | :error, String.t() | atom()}
+  def create(pid, engine_name, roleset_name, roleset_payload \\ test_payload()) do
+    create_client(pid, engine_name)
+    |> Engine.create_roleset(roleset_name, roleset_payload)
+  end
+
+
+
+  @doc """
+  Generates an `access token`/`service account key` from the given roleset
+  """
+  @spec read(pid(), atom(), gcp_secret_type, String.t()) :: {:ok, map()} | {:error, String.t()}
   def read(pid, engine_name, secret_type, roleset_name) do
     client = create_client(pid, engine_name)
-
     case secret_type do
       :access_token -> client |> Engine.gen_token(roleset_name)
       :service_account_key -> client |> Engine.gen_key(roleset_name)
@@ -30,15 +118,35 @@ defmodule Ptolemy.Engines.GCP do
     end
   end
 
-  @spec update(pid, atom, binary, map) :: map
-  def update(pid, engine_name, token_name, roleset_payload \\ test_payload()) do
-    create(pid, engine_name, token_name, roleset_payload)
+
+  @doc """
+  Updates a roleset by simply calling `create` with the new payload. This changes the
+  account email. Note that once a roleset is created, only the attributes `bindings` and `token_scopes`
+  can be changed.
+
+  If you are unsure of a roleset's configuration, it is recommended that you use the
+  function `read_roleset!`, update the resulting map, and then call `update` with the
+  new map to ensure that you are modifying only the editable attributes.
+  """
+  @spec update(pid(), atom(), String.t(), roleset) ::  {:ok | :error, String.t() | atom()}
+  def update(pid, engine_name, roleset_name, roleset_payload \\ test_payload()) do
+    create(pid, engine_name, roleset_name, roleset_payload)
   end
 
-  @spec delete(pid, atom, gcp_secret_type, binary) :: map
+  @doc """
+  Rotates the roleset account by deleting it and creating an identical one with a
+  different service account email (and Key ID). This invalidates keys previously
+  generated by the roleset.
+  """
+  @spec delete(pid(), atom(), gcp_secret_type, String.t()) ::  {:ok | :error, String.t() | atom()}
   def delete(pid, engine_name, secret_type, roleset_name) do
-    create_client(pid, engine_name)
-    |> Engine.gen_token(roleset_name)
+    client = create_client(pid, engine_name)
+
+    case secret_type do
+      :access_token -> client |> Engine.rotate_roleset_key(roleset_name)
+      :service_account_key -> client |> Engine.rotate_roleset(roleset_name)
+      _ -> {:error, "Unrecognized GCP secret type"}
+    end
   end
 
   @doc false
@@ -56,7 +164,8 @@ defmodule Ptolemy.Engines.GCP do
   end
 
   @doc """
-  Creates a Tesla Client with 
+  Creates a Tesla Client whose base URL refers to the given GCP engine.
+  The GCP Engine requires this client to make API calls to the correct engine
   """
   @spec create_client(pid, atom) :: map
   def create_client(pid, engine_name) do
@@ -69,46 +178,41 @@ defmodule Ptolemy.Engines.GCP do
       |> Map.fetch!(engine_name)
       |> Map.fetch!(:path)
 
-    IO.inspect(
-      Tesla.client([
-        {Tesla.Middleware.BaseUrl, "#{url}/v1/#{engine_path}"},
-        {Tesla.Middleware.Headers, creds},
-        {Tesla.Middleware.Timeout, timeout: 20_000},
-        {Tesla.Middleware.JSON, []}
-      ])
-    )
+    adapter = {Tesla.Adapter.Hackney, [ssl_options: [{:versions, [:"tlsv1.2"]}], recv_timeout: 20_000]}
+
+
+    Tesla.client([
+      {Tesla.Middleware.BaseUrl, "#{url}/v1/#{engine_path}"},
+      {Tesla.Middleware.Headers, creds},
+      {Tesla.Middleware.Timeout, timeout: 20_000},
+      {Tesla.Middleware.JSON, []}
+    ], adapter)
   end
 
   @doc """
-    Creates a map that represents a GCP role set 
-
-    ## Fields
-    * `:secret_type` - type of secret generated for this role set. i.e. "access_token", "service_account_key"
-    * `:project` - name of the GCP project to which this roleset's service account will belong 
-    * `:bindings` - bindings configuration string (read more here: https://www.vaultproject.io/docs/secrets/gcp/index.html#roleset-bindings)
-    * `:token_scopes` - **Applies only if secret type is "access_token"** list of OAuth scopes to assign to secrets generated under this role set
+  Generates type `roleset` from inputs
   """
-  @spec create_roleset(gcp_secret_type, binary, binary, [binary]) :: map
+
+  @spec create_roleset(gcp_secret_type, String.t(), String.t(), list(String.t())) :: roleset
   def create_roleset(secret_type, project, bindings, scopes \\ []) do
-    payload =
-      case secret_type do
-        :access_token ->
-          %{
-            secret_type: "access_token",
-            project: project,
-            bindings: bindings,
-            token_scopes: scopes
-          }
+    case secret_type do
+      :access_token ->
+        %{
+          secret_type: "access_token",
+          project: project,
+          bindings: bindings,
+          token_scopes: scopes
+        }
 
-        :service_account_key ->
-          %{
-            secret_type: "service_account_key",
-            project: project,
-            bindings: bindings
-          }
+      :service_account_key ->
+        %{
+          secret_type: "service_account_key",
+          project: project,
+          bindings: bindings
+        }
 
-        _ ->
-          {:error, "unrecognized gcp secret type"}
-      end
+      _ ->
+        {:error, "Unrecognized GCP secret type"}
+    end
   end
 end
--- lib/engines/gcp/gcp_engine.ex
+++ lib/engines/gcp/gcp_engine.ex
@@ -1,41 +1,116 @@
 defmodule Ptolemy.Engines.GCP.Engine do
   @moduledoc """
-  `Ptolemy.Engines.GCP.Engine` provides low level interaction with the Vault GCP Engine API
+  `Ptolemy.Engines.GCP.Engine` provides low level API interaction with the Vault GCP Secrets Engine
+
+  More information on the API this implements can be found at https://www.vaultproject.io/api/secret/gcp/index.html
   """
+
+  alias Ptolemy.Engines.GCP
+
+  @doc """
+  Submits a POST request to create a roleset
+  """
+  @spec create_roleset(Tesla.Client.t(), String.t(), GCP.roleset()) ::
+          {:ok | :error, String.t() | atom()}
+  def create_roleset(client, name, payload) do
+    with {:ok, resp} <- Tesla.post(client, "/roleset/#{name}", payload) do
+      case {resp.status, resp.body} do
+        {status, _body} when status in 200..299 ->
+          {:ok, "Roleset implemented"}
+
+        {status, body} ->
+          message = Map.fetch!(body, "errors")
+          {:error, "Roleset creation failed, Status: #{status} with error: #{message}"}
+      end
+    else
+      err -> err
+    end
+  end
+
+  @doc """
+  Submits a GET request to retrieve the configuration of a given roleset.
+  """
+  @spec read_roleset(Tesla.Client.t(), String.t()) :: {:ok, map()} | {:error, String.t() | atom()}
   def read_roleset(client, roleset_name) do
     with {:ok, resp} <- Tesla.get(client, "/roleset/#{roleset_name}") do
       case {resp.status, resp.body} do
-        {200, body} -> {:ok, body}
-        {status, body} -> {:error, body}
+        {status, body} when status in 200..299 ->
+          {:ok, body["data"]}
+
+        {status, body} ->
+          message = Map.fetch!(body, "errors")
+          {:error, "Reading roleset failed, Status: #{status} with error: #{message}"}
       end
     end
   end
 
-  # equivalent to invalidating the tokens under that roleset
+  @doc """
+  Submits a POST request to retrieve the configuration of a given roleset.
+  """
+  @spec rotate_roleset(Tesla.Client.t(), String.t()) :: {:ok | :error, String.t() | atom()}
   def rotate_roleset(client, roleset_name) do
     with {:ok, resp} <- Tesla.post(client, "/roleset/#{roleset_name}/rotate", %{}) do
       case {resp.status, resp.body} do
-        {204, body} -> {:ok, body}
-        {status, body} -> {:error, body}
+        {status, body} when status in 200..299 ->
+          {:ok, body}
+
+        {status, body} ->
+          message = Map.fetch!(body, "errors")
+          {:error, "Rotate roleset failed, Status: #{status} with error: #{message}"}
       end
     end
   end
 
+  @spec rotate_roleset_key(Tesla.Client.t(), String.t()) :: {:ok | :error, String.t() | atom()}
   def rotate_roleset_key(client, roleset_name) do
     with {:ok, resp} <- Tesla.post(client, "/roleset/#{roleset_name}/rotate-key", %{}) do
       case {resp.status, resp.body} do
-        {204, body} -> {:ok, body}
-        {status, body} -> {:error, body["errors"]}
+        {status, body} when status in 200..299 ->
+          {:ok, body}
+
+        {status, body} ->
+          message = Map.fetch!(body, "errors")
+          {:error, "Rotate roleset-key failed, Status: #{status} with error: #{message}"}
       end
     end
   end
 
+  @doc """
+  Submits a GET request to retrieve a temporary Oauth2 from an `access_token` roleset.
+  """
+  @spec gen_token(Tesla.Client.t(), String.t()) :: {:ok, map()} | {:error, String.t() | atom()}
   def gen_token(client, roleset_name) do
-    Tesla.get(client, "token/#{roleset_name}")
+    with {:ok, resp} <- Tesla.get(client, "token/#{roleset_name}") do
+      case {resp.status, resp.body} do
+        {status, body} when status in 200..299 ->
+          {:ok, body["data"]}
+
+        {status, body} ->
+          message = Map.fetch!(body, "errors")
+          {:error, "Generating Oauth2 token failed, Status: #{status} with error: #{message}"}
+      end
+    else
+      err -> err
+    end
   end
 
+  @doc """
+  Submits a GET request to retrieve a temporary token from an `access_token` roleset.
+  """
+  @spec gen_key(Tesla.Client.t(), String.t()) :: {:ok, map()} | {:error, String.t() | atom()}
   def gen_key(client, roleset_name) do
-    Tesla.get(client, "key/#{roleset_name}")
+    with {:ok, resp} <- Tesla.get(client, "key/#{roleset_name}") do
+      case {resp.status, resp.body} do
+        {status, body} when status in 200..299 ->
+          {:ok, body["data"]}
+
+        {status, body} ->
+          message = Map.fetch!(body, "errors")
+          {:error, "Generating svc acc key failed, Status: #{status} with error: #{message}"}
+      end
+    else
+      err -> err
+    end
   end
 
   def create_roleset(client, name, payload) do
