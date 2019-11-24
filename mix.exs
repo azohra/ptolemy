b599cdc98ad355a91fc05f8b7531a8a039e252bf
Brandon Sam Soon <brandon.samsoon@loblaw.ca>
2019-11-24 17:29:49 -0500

    version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ptolemy.MixProject do
   def project do
     [
       app: :ptolemy,
-      version: "0.2.1",
+      version: "0.3.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
